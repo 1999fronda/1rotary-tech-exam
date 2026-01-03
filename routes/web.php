25f717a3a919fa70914b00203473822f7cf092a9
1999fronda <episcope14@gmail.com>
2026-01-04 00:11:57 +0800

    Added auth routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('auth.login');
+Route::middleware('guest')->group(function () {
+    Route::get('/', [AuthController::class, 'showLogin'])->name('auth.login.show');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('auth.register.show');
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 });
 
-Route::get('/register', function () {
-    return view('auth.register');
+Route::middleware('auth')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
