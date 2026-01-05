37559a5fb7be2425ddb87c787f6eaf226be893f2
1999fronda <episcope14@gmail.com>
2026-01-05 09:55:54 +0800

    Updated tasks migration; made project_id and name unique

---
+++ database/migrations/2026_01_05_015256_add_unique_project_task_name_to_tasks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->unique(['project_id', 'name'], 'tasks_project_id_name_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            $table->dropUnique('tasks_project_id_name_unique');
+        });
+    }
+};
